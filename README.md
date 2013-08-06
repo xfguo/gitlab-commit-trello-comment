fcbeeb9713a335a280b23442dd9ac3e3a767f4d9
Xiongfei(Alex) Guo <xfguo@credosemi.com>
2013-08-06 21:26:38 +0800

    Modify gitlab-webhook-receiver prjoect to gitlab-commit-trello-comment.

--- README.md
+++ README.md
@@ -1,70 +1,35 @@
 # Introduction
 
+gitlab-commit-trello-comment will monitor your gitlab commit with card number(like #234) and push a format comment to that card of your trello board.
+modified from gitlab-webhook-receiver project.
+
 gitlab-webhook-receiver is a script to receive http posts from gitlab and then
 pull the latest branches from a git repo.
 
-
 # License
 
-gitlab-webhook-receiver is released under the [GPL v2](http://www.gnu.org/licenses/gpl-2.0.html).
-
+gitlab-commit-trello-comment is released under the [GPL v2](http://www.gnu.org/licenses/gpl-2.0.html).
 
 # Documentation
 
 (1) Modify the script
 ---------------------
 
-Modify the script near the top where it looks like this:
-<pre>
-############################################################
-##### You will likely need to change some of the below #####
-
-# log file for this script
-log_file = '/var/lib/puppet/gitlab-webhook-receiver/webhook.log'
-
-# where the puppet base git dir is
-git_dir = "/etc/puppet/environments"
-
-# the puppet master environment
-git_master_dir = "/etc/puppet/environments/master"
-
-# this is the name of the gitlab project name
-git_project = "newpuppet"
-
-log_max_size = 25165824         # 24 MB
-log_level = logging.INFO
-#log_level = logging.DEBUG      # DEBUG is quite verbose
-
-##### You should stop changing things unless you know what you are doing #####
-##############################################################################
-</pre>
-
-update it with where your dirs live.
-
+Copy the config.py.sample to config and fill your gitlab and trello info:
 
 (2) create the gitlab webhook
 -----------------------------
 
-In gitlab, as admin, go to "Hooks" tab, create hook as:
-http://your.ip.goes.here:8000
+In gitlab, as admin, go to "Hooks" tab, create hook as: http://your.ip.goes.here:8000
 
 or change the port on line 175 of the script.
-
 
 (3) Optional init script
 ------------------------
 
 remember to edit the script if any of your directories were changed.
 
-
 # Trouble getting it working?
 
-Let me know what's happening and I'll try to help. Email me at
-shawn@systemtemplar.org.
+Let me know what's happening and I'll try to help. Email me at xfguo@credosemi.com.
 
-
-# Contributing
-
-I'm open to any feedback / patches / suggestions.
-
-Shawn Sterling shawn@systemtemplar.org
---
+++ gitlab-commit-trello-comment.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python -tt
+#
+# Copyright (C) 2012 Shawn Sterling <shawn@systemtemplar.org>
+# Copyright (C) 2013 Xiongfei(Alex) Guo <xfguo@credosemi.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+# gitlab-commit-trello-comment: a script which push gitlab commit to trello card comment.
+# modify from gitlab-webhook-receiver: a script for gitlab & puppet integration
+#
+# The latest version of git will be found on:
+# https://github.com/xfguo/gitlab-commit-trello-comment
+#
+# The latest version of gitlab-webhook-receiver will be found on shawn-sterling's github page:
+# https://github.com/shawn-sterling/gitlab-webhook-receiver
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import os
+import re
+import json
+import logging
+import logging.handlers
+
+from trolly import client as trolly_client
+from trolly import board as trolly_board
+from trolly import card
+
+import config
+
+import pprint
+
+log = logging.getLogger('log')
+log.setLevel(config.log_level)
+log_handler = logging.handlers.RotatingFileHandler(config.log_file,
+                                                   maxBytes=config.log_max_size,
+                                                   backupCount=4)
+f = logging.Formatter("%(asctime)s %(filename)s %(levelname)s %(message)s",
+                      "%B %d %H:%M:%S")
+log_handler.setFormatter(f)
+log.addHandler(log_handler)
+
+class webhookReceiver(BaseHTTPRequestHandler):
+    def comment_to_trello(self, card_short_id, comment):
+        log.debug("Try comment on card #%d, [\n%s\n]" % (card_short_id, comment))
+        trello_key = config.trello_key
+        trello_token = config.trello_token
+        board_id = config.board_id
+        
+        client = trolly_client.Client(trello_key, user_auth_token = trello_token)
+        board = trolly_board.Board(client, board_id)
+        card = board.getCard(str(card_short_id))
+        card.addComments(comment)
+
+    def do_POST(self):
+        """
+            receives post, handles it
+        """
+        log.debug('got post')
+        post = json.load(self.rfile)
+        
+        repo = post['repository']['name']
+        repo_url = config.gitlab_url + '/%s' % repo
+        branch = re.split('/', post['ref'])[-1]
+        branch_url = repo_url + '/commits/%s' % branch
+        log.debug(pprint.pformat(post))
+        for commit in post['commits']:
+            card_short_id_list = map(int, re.findall('#([1-9]+)', commit['message']))
+            git_hash = commit['id'][:7]
+            git_hash_url = repo_url + '/commit/%s' % git_hash
+            author = commit['author']['name']
+            comment = commit['message']
+            trello_comment = '''\[**%s** has a new commit about this card\]
+\[repo: [%s](%s) | branch: [%s](%s) | hash: [%s](%s)\]
+----
+%s''' % (author, repo, repo_url, branch, branch_url, git_hash, git_hash_url, comment)
+            for card_short_id in card_short_id_list:
+                self.comment_to_trello(card_short_id, trello_comment)
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write('OK')
+        self.wfile.write('\n')
+
+def main():
+    """
+        the main event.
+    """
+    try:
+        server = HTTPServer(('', 9000), webhookReceiver)
+        log.info('started web server...')
+        server.serve_forever()
+    except KeyboardInterrupt:
+        log.info('ctrl-c pressed, shutting down.')
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
